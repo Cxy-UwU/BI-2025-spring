355f24f9ca91732069023aedce9fc0df7dabc7b8
wfy3364 <2489589935@qq.com>
2025-06-12 23:35:11 +0800

    修改数据库属性

--- app/models.py
+++ app/models.py
@@ -2,37 +2,39 @@ from . import db
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
 
 class News(db.Model):
     __tablename__ = 'news'
-    id = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String(50))
-    topic = db.Column(db.String(50))
-    title = db.Column(db.String(200))
+    id = db.Column(db.String, primary_key=True)
+    category = db.Column(db.String(100))
+    topic = db.Column(db.String(100))
+    title = db.Column(db.String(1000))
     content = db.Column(db.Text)
     first_read_time = db.Column(db.DateTime)
 
 class Entity(db.Model):
     __tablename__ = 'entity'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(255)) 
     description = db.Column(db.Text)
 
 class NewsEntity(db.Model):
     __tablename__ = 'news_entity'
-    n_id = db.Column(db.Integer, db.ForeignKey('news.id'), primary_key=True)
+    n_id = db.Column(db.String, db.ForeignKey('news.id'), primary_key=True)
     e_id = db.Column(db.Integer, db.ForeignKey('entity.id'), primary_key=True)
 
 class Click(db.Model):
     __tablename__ = 'click'
-    u_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    n_id = db.Column(db.Integer, db.ForeignKey('news.id'), primary_key=True)
-    time = db.Column(db.DateTime)
-    dwell = db.Column(db.Integer) 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    u_id = db.Column(db.String, db.ForeignKey('user.id'), primary_key=True)
+    n_id = db.Column(db.String, db.ForeignKey('news.id'), primary_key=True)
+    time = db.Column(db.DateTime, primary_key=True)
+    dwell = db.Column(db.Integer)
 
 class Skip(db.Model):
     __tablename__ = 'skip'
-    u_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    n_id = db.Column(db.Integer, db.ForeignKey('news.id'), primary_key=True)
-    time = db.Column(db.DateTime)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    u_id = db.Column(db.String, db.ForeignKey('user.id'), primary_key=True)
+    n_id = db.Column(db.String, db.ForeignKey('news.id'), primary_key=True)
+    time = db.Column(db.DateTime, primary_key=True)
