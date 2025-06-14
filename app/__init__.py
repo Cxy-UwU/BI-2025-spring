d4b4af66f4689fdcdfe5674754166b442bd4862a
Rock1ey <2061218098@qq.com>
2025-06-15 00:15:29 +0800

    用户兴趣查询api（可能还需优化）

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from .config import Config
 
 db = SQLAlchemy()
 
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -19,8 +18,11 @@ def create_app():
         from .routes.news import news_bp
         from .routes.exposure import exposure_bp
         from .routes.users import users_bp
+        from .routes.interest import interest_bp
         app.register_blueprint(news_bp, url_prefix='/api/news')
         app.register_blueprint(exposure_bp, url_prefix='/api/exposure')
         app.register_blueprint(users_bp, url_prefix='/api/users')
+        app.register_blueprint(interest_bp, url_prefix='/api/interest')
+
 
     return app
